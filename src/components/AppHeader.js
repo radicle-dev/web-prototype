5334253a3475e2582021277f78fa08334494acef
Julien Donck <donckjulien@gmail.com>
2018-06-20 00:19:11 +0200

    updated app header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,13 +3,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: 8fr 1fr 1fr;
+  grid-template-rows: 56px;
   height: 64px;
   color: white;
   border-radius: 3px;
-  padding-left: 24px;
+  padding: 0 24px;
+  align-items: center;
   > h1 {
     font-size: 1.5em;
     font-weight: bold;
+  }
+  > p {
+    text-align: right;
+    color: grey;
   }
 `;
 const Circle = styled.header`
@@ -17,7 +26,7 @@ const Circle = styled.header`
   display: inline-block;
   width: 0.4em;
   height: 0.4em;
-  border: 0.05em solid white;
+  border: 0.03em solid white;
   position: relative;
   border-radius: 3em;
 `;
@@ -25,6 +34,8 @@ const Circle = styled.header`
 const AppHeader = () => (
   <HeaderContainer>
     <Circle />
+    <p>Issues</p>
+    <p>Wallet</p>
   </HeaderContainer>
 );
 

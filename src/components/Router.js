5242b50efa1488151534c8426b00aa3ac446460e
Julien Donck <donckjulien@gmail.com>
2018-06-19 20:57:32 +0200

    Revert "remove unused chart.js component"
    This reverts commit eea3a01d3a1a06bb6a8c58281da4664143e3b289.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from '../App';
-import RepoPage from './RepoPage';
+import Currency from './Currency';
 import NotFound from './NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/repo/:repoId" component={RepoPage} />
+      <Route exact path="/currency/:currencyId" component={Currency} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>

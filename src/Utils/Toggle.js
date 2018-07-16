58dddde9f6b8d55459b3c61bb9d98848112ffc92
Julien Donck <donckjulien@gmail.com>
2018-07-17 00:35:33 +0200

    adding utils

---
+++ src/Utils/Toggle.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Toggle extends Component {
+  state = {
+    on: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      on: !this.state.on,
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    return children({
+      on: this.state.on,
+      toggle: this.toggle,
+    });
+  }
+}
+
+Toggle.propTypes = {
+  children: PropTypes.func.isRequired,
+};
---
+++ src/Utils/colors.js
@@ -0,0 +1,11 @@
+export const lightGrey = '#f2f2f2';
+export const grey = '#d8d8d8';
+export const black = '#333';
+export const teal = '#82d8d8';
+
+export default {
+  lightGrey,
+  grey,
+  black,
+  teal,
+};

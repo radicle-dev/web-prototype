52877241280bbd29ff5b8ef1f96953313e2fbb03
Julien Donck <donckjulien@gmail.com>
2018-07-24 09:47:35 +0200

    checking if data is loaded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import OrgPage from './Components/OrgPage';
 import RepoPage from './Components/RepoPage';
@@ -18,22 +18,25 @@ class App extends Component {
   async fetchRepos() {
     await (await fetch(url)).json().then(data => {
       this.setState({ data });
-      console.log(this.state.data);
     });
   }
 
   render() {
     const { data } = this.state;
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={props => <OrgPage {...props} data={data} />} />
-          <Route exact path="/repo/:repoId" render={props => <RepoPage {...props} data={data} />} />
-          <Route exact path="/juliendonck" render={() => <h1>juliendonck</h1>} />
-          <Route exact path="/daimler" render={() => <h1>daimler</h1>} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Fragment>
+        {data && (
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" render={props => <OrgPage {...props} data={data} />} />
+              <Route exact path="/repo/:repoId" render={props => <RepoPage {...props} data={data} />} />
+              <Route exact path="/juliendonck" render={() => <h1>juliendonck</h1>} />
+              <Route exact path="/daimler" render={() => <h1>daimler</h1>} />
+              <Route component={NotFound} />
+            </Switch>
+          </BrowserRouter>
+        )}
+      </Fragment>
     );
   }
 }
